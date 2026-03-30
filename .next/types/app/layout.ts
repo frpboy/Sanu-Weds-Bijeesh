b6a5cf2058dccb53081d317389f5068d053fd35c
frpboy <frpboy12@gmail.com>
2026-03-30 19:33:09 +0530

    fix: update server manifests and remove unused font files
    - Updated the interception route rewrite manifest to remove trailing newline.
    - Refactored the middleware build manifest for improved structure and added missing files.
    - Adjusted the middleware react loadable manifest for consistency in file paths.
    - Reformatted the next font manifest for better readability.
    - Enhanced the server reference manifest with proper formatting.
    - Reformatted the server reference manifest JSON for clarity.
    - Updated the webpack runtime with detailed comments for better understanding.
    - Removed unused font files from the static media directory.
    - Corrected file paths in layout and page type definitions to use consistent casing.

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: e:\K4NN4N\Sanu-Weds-Bijeesh\src\app\layout.tsx
+// File: E:\K4NN4N\Sanu-Weds-Bijeesh\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: e:\K4NN4N\Sanu-Weds-Bijeesh\src\app\page.tsx
+// File: E:\K4NN4N\Sanu-Weds-Bijeesh\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
